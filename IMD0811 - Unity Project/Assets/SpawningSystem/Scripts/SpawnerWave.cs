0a6c6b8329135f601fa67ed24a1138c4d3d02667
Gabriel Lucena <gabriellucena23@gmail.com>
2019-10-30 16:20:33 -0300

    Added SpawningSystem.cs

---
+++ IMD0811 - Unity Project/Assets/SpawningSystem/Scripts/SpawnerWave.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu(fileName = "New Wave", menuName = "Spawner Wave")]
+public class SpawnerWave : ScriptableObject
+{
+    [FormerlySerializedAs("enemiesToSpawn")] 
+    [SerializeField] GameObject[] elementsToSpawn;
+    [SerializeField] int[] amountToSpawn;
+    [SerializeField] float[] timeBetweenSpawn;
+    
+    public int NumOfElements() { return elementsToSpawn.Length; }
+    public GameObject GetElement(int pos) { return elementsToSpawn[pos]; }
+    public float GetSpawnInterval(int pos) { return timeBetweenSpawn[pos]; }
+    public int GetSpawnAmount(int pos) { return amountToSpawn[pos]; }
+}
