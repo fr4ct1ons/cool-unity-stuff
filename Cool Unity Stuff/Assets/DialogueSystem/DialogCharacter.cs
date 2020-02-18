822058dff9e01d97c11a8fb6681358d98da3cfb6
lucena-fr4ct1ons <gabriellucena23@gmail.com>
2020-02-17 22:19:51 -0300

    Update to Unity 2019.3.1f1

---
+++ Cool Unity Stuff/Assets/DialogueSystem/DialogCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Dialog Character", menuName = "Dialog Character")]
+public class DialogCharacter : ScriptableObject
+{
+    [Tooltip("Name of the character.")]
+    [SerializeField] string characterName;
+    [Tooltip("List of expressions the character has. 0 is the default.")]
+    [SerializeField] Sprite[] expressions;
+    [Tooltip("Text color of the character.")]
+    [SerializeField] Color32 textColor = Color.white;
+
+    public Sprite GetSprite(uint index) { return expressions[index]; }
+    public string GetName() { return characterName; }
+    public Color32 GetColor() { return textColor; }
+}
